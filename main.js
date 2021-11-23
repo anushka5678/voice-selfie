9d6a7f19aff67e20e08bfc999e7ff5439cec6089
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2021-11-23 16:24:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,14 +15,24 @@ recognition.onresult = function run(event) {
 
     document.getElementById("textbox").innerHTML= Content;
 
-    speak();
+    if(Content == "take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+    }
 }
 function speak(){
     var synth= window.speechSynthesis;
-    var speak_data= document.getElementById("textbox").value;
+    var speak_data= "Taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    },5000);
+
 }
 Webcam.set({
     width:360,
@@ -31,3 +41,15 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="selfie_image" src="' +data_uri+'">';
+    });
+}
+function save(){
+    link= document.getElementById("link");
+    image= document.getElementById("selfie_image").src;
+    link.href= image;
+    link.click();
+}
